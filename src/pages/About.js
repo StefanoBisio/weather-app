4e1f971a18564e0aa9f1774a95eb630b164b4c9d
Stefano Bisio <s.b.milano@gmail.com>
2023-03-27 00:36:09 +0100

    copy in About page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,10 +1,26 @@
 import React from "react"; 
+import styled from 'styled-components';
+
+const AboutContainer = styled.div`
+  h1 {
+    font-size: 1.2rem;
+    margin-bottom: 2rem;
+  }
+`;
 
 function About() {
     return (
-        <div>
-        <h1>About</h1>
-        </div>
+        <AboutContainer>
+            <h1>About</h1>
+            <p>This React app functions using the following:</p>
+            <ul>
+                <li>React.JS</li>
+                <li>React Router</li>
+                <li>Styled Components</li>
+                <li>Ipapi.co API</li>
+                <li>Weatherapi API</li>
+            </ul>
+        </AboutContainer>
     );
     }
 
